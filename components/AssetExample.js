b7722f385214d8a79c003fb930400d910bd9d327
xpowert <xpowertlatvia@gmail.com>
2021-10-22 14:53:15 +0300

    Update for task nr.2
    Updated for task nr.2. 
    Deleted orginal text.

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -4,9 +4,6 @@ import { Text, View, StyleSheet, Image } from 'react-native';
 export default function AssetExample() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Local files and assets can be imported by dragging and dropping them into the editor
-      </Text>
       <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
     </View>
   );
